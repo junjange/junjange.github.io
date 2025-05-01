1d77507f2ea6ae3d195c2999ba0fe650a5463a7b
JunJangE <wnswkd486@gmail.com>
2025-05-02 05:14:57 +0900

    feat: Section

---
+++ composeApp/src/wasmJsMain/kotlin/junjange/dev/ui/model/Section.kt
@@ -0,0 +1,19 @@
+package junjange.dev.ui.model
+
+import junjange_dev.composeapp.generated.resources.Res
+import junjange_dev.composeapp.generated.resources.section_about
+import junjange_dev.composeapp.generated.resources.section_career
+import junjange_dev.composeapp.generated.resources.section_contact
+import junjange_dev.composeapp.generated.resources.section_experience
+import junjange_dev.composeapp.generated.resources.section_home
+import org.jetbrains.compose.resources.StringResource
+
+enum class Section(
+    val title: StringResource,
+) {
+    Home(Res.string.section_home),
+    AboutMe(Res.string.section_about),
+    Career(Res.string.section_career),
+    Experience(Res.string.section_experience),
+    Contact(Res.string.section_contact),
+}
