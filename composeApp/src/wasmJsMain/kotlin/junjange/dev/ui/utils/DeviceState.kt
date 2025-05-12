7cdad21ed19da90a9c144e48943ed87c48426a9a
JunJangE <wnswkd486@gmail.com>
2025-05-12 20:35:03 +0900

    refactor: DeviceState 확장함수 수정

--- composeApp/src/wasmJsMain/kotlin/junjange/dev/ui/utils/DeviceState.kt
+++ composeApp/src/wasmJsMain/kotlin/junjange/dev/ui/utils/DeviceState.kt
@@ -20,19 +20,22 @@ typealias DeviceState = State<Device>
 
 @Composable
 fun rememberDeviceState(): DeviceState {
-    val screenWidthInDp = with(LocalDensity.current) { LocalScreenSize.current.width.toDp() }
-    return remember(screenWidthInDp) { derivedStateOf { Device.fromWidth(screenWidthInDp) } }
+    val screenWidthDp = with(LocalDensity.current) { LocalScreenSize.current.width.toDp() }
+    return remember(screenWidthDp) {
+        derivedStateOf { Device.fromWidth(screenWidthDp) }
+    }
 }
 
-val DeviceState.isMobile
+val DeviceState.isMobile: Boolean
     get() = value == Device.MOBILE
-val DeviceState.isPc
+
+val DeviceState.isPc: Boolean
     get() = value == Device.PC
 
 @Composable
 @ReadOnlyComposable
 fun DeviceState.fontSize(): TextUnit =
-    when (this.value) {
+    when (value) {
         Device.PC -> 72.sp
         Device.MOBILE -> 48.sp
     }
@@ -40,18 +43,18 @@ fun DeviceState.fontSize(): TextUnit =
 @Composable
 @ReadOnlyComposable
 fun DeviceState.contentPadding(): PaddingValues =
-    when (this.value) {
-        Device.MOBILE -> mobileSectionPaddingValues
-        Device.PC -> pcSectionPaddingValues
+    when (value) {
+        Device.PC -> PcContentPadding
+        Device.MOBILE -> MobileContentPadding
     }
 
-private val pcSectionPaddingValues =
+private val PcContentPadding =
     PaddingValues(
         horizontal = PC_CONTENT_HORIZONTAL_PADDING.dp,
         vertical = PC_CONTENT_VERTICAL_PADDING.dp,
     )
 
-private val mobileSectionPaddingValues =
+private val MobileContentPadding =
     PaddingValues(
         horizontal = MOBILE_CONTENT_HORIZONTAL_PADDING.dp,
         vertical = MOBILE_CONTENT_VERTICAL_PADDING.dp,
