5e48168256959ef061091c21bee5730011be9adb
JunJangE <wnswkd486@gmail.com>
2025-05-16 23:22:03 +0900

    chore: 문구 수정

--- composeApp/src/wasmJsMain/kotlin/junjange/dev/ui/model/Etc.kt
+++ composeApp/src/wasmJsMain/kotlin/junjange/dev/ui/model/Etc.kt
@@ -2,33 +2,33 @@ package junjange.dev.ui.model
 
 import junjange_dev.composeapp.generated.resources.Res
 import junjange_dev.composeapp.generated.resources.etc_devfest_campus_korea
-import junjange_dev.composeapp.generated.resources.etc_devfest_campus_korea_date
+import junjange_dev.composeapp.generated.resources.etc_devfest_campus_korea_period
 import junjange_dev.composeapp.generated.resources.etc_gdsc_kr_winter_cup
-import junjange_dev.composeapp.generated.resources.etc_gdsc_kr_winter_cup_date
+import junjange_dev.composeapp.generated.resources.etc_gdsc_kr_winter_cup_period
 import junjange_dev.composeapp.generated.resources.etc_gdsc_sch_x_skhy_hack
-import junjange_dev.composeapp.generated.resources.etc_gdsc_sch_x_skhy_hack_date
+import junjange_dev.composeapp.generated.resources.etc_gdsc_sch_x_skhy_hack_period
 import junjange_dev.composeapp.generated.resources.etc_kusitms
-import junjange_dev.composeapp.generated.resources.etc_kusitms_date
+import junjange_dev.composeapp.generated.resources.etc_kusitms_period
 import org.jetbrains.compose.resources.StringResource
 
 enum class Etc(
     val titleRes: StringResource,
-    val dateRes: StringResource,
+    val periodRes: StringResource,
 ) {
     Kusitms(
         titleRes = Res.string.etc_kusitms,
-        dateRes = Res.string.etc_kusitms_date,
+        periodRes = Res.string.etc_kusitms_period,
     ),
     GDSC_SCH_X_SKHU_HACK(
         titleRes = Res.string.etc_gdsc_sch_x_skhy_hack,
-        dateRes = Res.string.etc_gdsc_sch_x_skhy_hack_date,
+        periodRes = Res.string.etc_gdsc_sch_x_skhy_hack_period,
     ),
     GDSC_KR_WINTER_CUP(
         titleRes = Res.string.etc_gdsc_kr_winter_cup,
-        dateRes = Res.string.etc_gdsc_kr_winter_cup_date,
+        periodRes = Res.string.etc_gdsc_kr_winter_cup_period,
     ),
     DEVFEST_CAMPUS_KOREA(
         titleRes = Res.string.etc_devfest_campus_korea,
-        dateRes = Res.string.etc_devfest_campus_korea_date,
+        periodRes = Res.string.etc_devfest_campus_korea_period,
     ),
 }
